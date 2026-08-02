6113d1da444990bbec6cb5cc60f9e2eb644c126d
Shakti Lucidia <shakti_lucidia@proton.me>
2026-08-02 02:54:27 -0300

    RegistrationResult to HTTP response and vice-versa. Hope it will work.

--- Backend/backend/ratel_backend_users/ratel_backend_users_dtos/Registration/Extensions/RegistrationResultExtension.cs
+++ Backend/backend/ratel_backend_users/ratel_backend_users_dtos/Registration/Extensions/RegistrationResultExtension.cs
@@ -14,9 +14,12 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using System.Net;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ratel_backend_users_dtos.Registration.Enums;
+using ratel_shared_auxiliary.Extensions;
 
 namespace ratel_backend_users_dtos.Registration.Extensions;
 
@@ -25,6 +28,16 @@ namespace ratel_backend_users_dtos.Registration.Extensions;
 /// </summary>
 public static class RegistrationResultExtension
 {
+    /// <summary>
+    /// Problem with this URL is being returned when login is taken
+    /// </summary>
+    private const string LoginIsTakenProblemType = "https://ratel.chat/problems/registration/login-is-taken";
+
+    /// <summary>
+    /// Problem with this URL is being returned when some validations failed during registration
+    /// </summary>
+    private const string ValidationFailedProblemType = "https://ratel.chat/problems/registration/failed-validation";
+
     /// <summary>
     /// Fields, on what validation may fail
     /// </summary>
@@ -82,6 +95,22 @@ public static class RegistrationResultExtension
     };
 
     /// <summary>
+    /// Maps pairs "field and description" to registration errors
+    /// </summary>
+    private static readonly IReadOnlyDictionary<(string Field, string Description), RegistrationError> _validationProblemsToErrors =
+        _errorsToFieldsNames
+        .Keys
+        .ToDictionary
+        (
+            error =>
+            (
+                Field: _errorsToFieldsNames[error],
+                Description: _errorsToDescriptions[error]
+            ),
+            error => error
+        );
+
+    /// <summary>
     /// Convert registration result to action result
     /// </summary>
     public static IActionResult ToActionResult(this ControllerBase controller, IReadOnlySet<RegistrationError> errors)
@@ -94,13 +123,13 @@ public static class RegistrationResultExtension
         if (errors.Contains(RegistrationError.FailedLoginTaken))
         {
             return controller
-                    .UnprocessableEntity
+                    .Conflict
                     (
                         new ProblemDetails
                         {
-                            Type = "https://ratel.chat/problems/registration/login-is-taken",
-                            Title = "Login is empty",
-                            Status = StatusCodes.Status422UnprocessableEntity
+                            Type = LoginIsTakenProblemType,
+                            Title = "Login is taken",
+                            Status = StatusCodes.Status409Conflict
                         }
                     );
         }
@@ -111,7 +140,7 @@ public static class RegistrationResultExtension
             .Overlaps
             (
                 [
-                        RegistrationError.FailedLoginTaken
+                    RegistrationError.FailedLoginTaken
                 ]
             )
         )
@@ -141,25 +170,98 @@ public static class RegistrationResultExtension
             {
                 Status = StatusCodes.Status422UnprocessableEntity,
                 Title = "Registration failed",
-                Type = "https://ratel.im/problems/registration/failed-validation"
+                Type = ValidationFailedProblemType
             }
         );
     }
 
-    // /// <summary>
-    // /// Convert registration server response to registration result
-    // /// </summary>
-    // public static RegistrationResult ToResult(this HttpResponseMessage response)
-    // {
-    //     return response.StatusCode switch
-    //     {
-    //         System.Net.HttpStatusCode.Created => RegistrationResult.Created,
+    public static async Task<IReadOnlySet<RegistrationError>> ToRegistrationResult
+    (
+        this HttpResponseMessage response,
+        CancellationToken cancellationToken = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(response);
 
-    //         System.Net.HttpStatusCode.UnprocessableEntity => RegistrationResult.FailedPasswordTooWeak,
+        return response.StatusCode switch
+        {
+            HttpStatusCode.Created => new HashSet<RegistrationError>(),
 
-    //         System.Net.HttpStatusCode.Conflict => RegistrationResult.FailedLoginTaken,
+            HttpStatusCode.Conflict => await ParseConflictAsync(response, cancellationToken),
 
-    //         _ => throw new ArgumentOutOfRangeException(nameof(response))
-    //     };
-    // }
+            HttpStatusCode.UnprocessableEntity => await ParseValidationErrorsAsync(response, cancellationToken),
+
+            _ => throw new InvalidOperationException
+                (
+                    "Unexpected HTTP status code in registration response: { (int)response.StatusCode } ({ response.StatusCode })"
+                )
+        };
+    }
+
+    private static async Task<IReadOnlySet<RegistrationError>> ParseConflictAsync
+    (
+        HttpResponseMessage response,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var problemDetails = await response
+            .Content
+            .ReadFromJsonAsync<ProblemDetails>(cancellationToken)
+            ??
+            throw new InvalidOperationException("Registration conflict response contains no ProblemDetails");
+
+        return problemDetails.Type switch
+        {
+            LoginIsTakenProblemType => new HashSet<RegistrationError>
+            {
+                RegistrationError.FailedLoginTaken
+            },
+
+            _ => throw new InvalidOperationException($"Unknown registration conflict problem type: '{ problemDetails.Type }'")
+        };
+    }
+
+    private static async Task<IReadOnlySet<RegistrationError>> ParseValidationErrorsAsync
+    (
+        HttpResponseMessage response,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var problemDetails = await response
+            .Content
+            .ReadFromJsonAsync<ValidationProblemDetails>(cancellationToken)
+            ??
+            throw new InvalidOperationException("Registration validation response contains no ValidationProblemDetails");
+
+
+        if (problemDetails.Type != ValidationFailedProblemType)
+        {
+            throw new InvalidOperationException($"Unknown registration validation problem type: '{ problemDetails.Type }'");
+        }
+
+        var result = new HashSet<RegistrationError>();
+
+        foreach (var (field, descriptions) in problemDetails.Errors)
+        {
+            foreach (var description in descriptions)
+            {
+                if (!_validationProblemsToErrors.TryGetValue((field, description), out var error))
+                {
+                    throw new InvalidOperationException
+                    (
+                        $"Unknown registration validation error: Field='{ field }', Description='{ description }'"
+                    );
+                }
+
+                result.AddUnique(error);
+            }
+        }
+
+        if (!result.Any())
+        {
+            throw new InvalidOperationException("Registration validation response contains no errors");
+        }
+
+        return result;
+    }
 }
