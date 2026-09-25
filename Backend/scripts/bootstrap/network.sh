9ddf374e6d7e04989951f2a15f82f964f572ae0a
Shakti Lucidia <shakti_lucidia@proton.me>
2026-09-25 01:11:45 -0300

    Fixed networks addresses

--- Backend/scripts/bootstrap/network.sh
+++ Backend/scripts/bootstrap/network.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker network inspect kind > /dev/null 2>&1 || docker network create --driver bridge --ipv6 --subnet fd62:7261:7465:6c00::/64 kind
+docker network inspect kind > /dev/null 2>&1 || docker network create --driver bridge --ipv6 --subnet 172.18.0.0/16 --subnet fc00:f853:ccd:e793::/64 kind
 
 exit 0
